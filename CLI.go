e33805c7710f03933e33c5b8df49ed1c63e090f3
root <rickard@ignalina.dk>
2021-10-20 17:41:26 +0200

    fix

--- CLI.go
+++ CLI.go
@@ -31,7 +31,7 @@ func main() {
 	start := time.Now()
     if(len(os.Args)!=8) {
 
-		println("Shredder (Unreleased Betha 2021-10-21)")
+		println("Shredder (Unreleased Betha 2021-10-21 17:39)")
 		println("Syntax       : shredder.exe <kafka broker> <schemaregistry> <schema file url> <schema id> <topic> <cores=partitions> <data file> ")
 		println("example usage: shredder.exe 10.1.1.90:9092 10.1.1.90:8081 schema1.json 5 tableXYZ_q123 1 test.data")
 		os.Exit(1)
